6756e365ced5338192e00148870ceb82fc2ea4b5
ubgk <82390536+ubgk@users.noreply.github.com>
2024-09-06 15:37:28 +0200

    Update pink_balancer/inverse_dynamics.py
    add shebang and license information
    
    Co-authored-by: Stéphane Caron <stephane.caron@inria.fr>

--- pink_balancer/inverse_dynamics.py
+++ pink_balancer/inverse_dynamics.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# SPDX-License-Identifier: Apache-2.0
+
 from functools import cache, cached_property
 from typing import Literal, Optional, cast
 
